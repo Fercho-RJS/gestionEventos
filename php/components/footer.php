b3aeef75f32ff0c93608e12f295490e048a410f8
David Rojas <fernandorojas.contacto@gmail.com>
2025-07-02 16:51:47 -0300

    Se crearon las bases de las páginas estáticas

--- php/components/footer.php
+++ php/components/footer.php
@@ -1,4 +1,4 @@
 <footer class="small pt-5 border-top m-2">
   <p><i class="bi bi-c-circle"></i> 2025 - EventsWeb.net - <a href="#">Política de privacidad</a> - <a href="#">Condiciones de uso</a></p>
-  <p class="text-secondary">Página desarrollada por alumnos de 3er año de la Tecnicatura Superior en Desarrollo de Software & Páginas web - C.L. 2025</p>
+  <p class="text-secondary">Página desarrollada por los Alumnos de 3er año de la <b>Tecnicatura Superior en Desarrollo de Software & Páginas web</b> - <b>Fernando D. E. Rojas G.</b> & <b>Cesar O. D. Meier</b> - <b>C.L.</b> 2025</p>
 </footer>
